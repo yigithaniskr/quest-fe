b3f46d979197a043523caa39b9d9757bb2b87a6e
yigithaniskr <77109272+yigithaniskr@users.noreply.github.com>
2022-03-31 23:02:58 +0300

    Added project source code.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import User from "./components/User/User";
+import { Switch } from "react-router-dom";
+import Auth from "./components/Auth/Auth";
+import { Redirect } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar/>
+        <Switch >
+          <Route exact path="/" component={Home}></Route>
+          <Route exact path="/users/:userId" component={User}></Route>
+          <Route exact path="/auth">
+          {localStorage.getItem("currentUser") != null ? <Redirect to="/"/>: <Auth/>}
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
